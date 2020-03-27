a5299dd2f970dabd0d6d111dc6152c1764288d6b
andrew <asburr@hotmail.com>
2020-03-27 18:03:34 -0400

    Add time measurements to CLI.

--- src/magpie/Cli.java
+++ src/magpie/Cli.java
@@ -12,18 +12,39 @@ public class Cli {
     this.client=new Client(host,port,100,0xffee);
   }
   public void stdin() { try {
+    long before=0L,after=0L;
     System.out.print(">"); System.out.flush();
     BufferedReader reader=new BufferedReader(new InputStreamReader(System.in)); 
     String line = reader.readLine(); 
+    String lastLine=null;
+    String lastResp=null;
+    long ld=0L;
     while (line!=null) {
       if (line.equals("x")) return;
-      client.sendBuf_String(line);
-      try { Thread.sleep(100); } catch(Exception e) {}
-      while (client.read(100,TimeUnit.MILLISECONDS)) {
+      if (line.startsWith("*")) {
+        int count=Integer.parseInt(line.substring(2));
+        before=System.nanoTime();
+        while (count>0) {
+          client.sendBuf_String(lastLine);
+          while (!client.read(100,TimeUnit.MILLISECONDS));
+          client.recvBuf_GetString(-1);
+          count--;
+        }
+        after=System.nanoTime();
+      } else {
+        before=System.nanoTime();
+        client.sendBuf_String(line);
+        while (!client.read(100,TimeUnit.MILLISECONDS));
+        after=System.nanoTime();
         System.out.print(client.recvBuf_GetString(-1));
+        lastLine=line;
       }
       if (client.shutdown) { System.out.println("Server closed"); return; }
-      System.out.print(">"); System.out.flush();
+      long d=(after-before)/100000;
+      long p=0L;
+      if (ld > 0 && d > ld ) p=d/ld; else p=-(ld/d);
+      System.out.print(String.format("%03d",d)+" "+p+">"); System.out.flush();
+      ld=d;
       line = reader.readLine(); 
     }
   } catch (Exception e) {
