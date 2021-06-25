8235da3de85b49ad1bfdd0d755f700bcda314a1d
andrew <asburr@hotmail.com>
2021-06-25 14:21:37 -0400

    change of strategy: was a digit tree, now is a dictionary.

--- magpie/src/mdigittree.py
+++ magpie/src/mdigittree.py
@@ -1,43 +1,59 @@
 #!/usr/bin/env python3
+import sys
 
 
-class MDigitNode:
+class MDigitsNode:
     
     def __init__(self):
-        self.next = {}
-        self.obj = None
+        # <digit length>: ([<wildindex>], dict)
+        self.dicts = {}
 
     def add(self, obj: any, digits: str, i: int):
-        if i == len(digits):
-            self.obj = obj
-            return
-        c = digits[i]
-        if c in self.next:
-            self.next[c].add(obj, digits, i+1)
-        else:
-            n = MDigitNode()
-            n.add(obj, digits, i+1)
-            self.next[c] = n
+        l = len(digits)
+        if l not in self.dicts:
+            self.dicts[l] = ([], {})
+        wilds, d = self.dicts[l]
+        i = 0
+        newwild = []
+        while MDigitTree.wildchar in digits[i:]:
+            i = digits.index(MDigitTree.wildchar, i)
+            newwild.append(i)
+            i += 1
+        if newwild not in wilds:
+            wilds.append(newwild)
+        if digits in d:
+            raise Exception("Duplicate key " + digits)
+        d[digits] = obj
 
     def find(self, digits: str, i: int) -> any:
-        if i == len(digits):
-            return self.obj
-        c = digits[i]
-        if c in self.next:
-            obj = self.next[c].find(digits, i+1)
+        l = len(digits)
+        d = self.dicts.get(l)
+        if not d:
+            return None
+        wilds, d = d
+        obj = d.get(digits)
+        if obj:
+            return obj
+        for wild in wilds:
+            l = list(digits)
+            for idx in wild:
+                l[idx] = MDigitTree.wildchar
+            wilddigits = "".join(l)
+            obj = d.get(wilddigits)
             if obj:
                 return obj
-        if MDigitTree.wildchar in self.next:
-            return self.next[MDigitTree.wildchar].find(digits, i+1)
-
 
 class MDigitTree:
     wildchar = "X"
     def __init__(self):
-        self.head = MDigitNode()
+        self.head = MDigitsNode()
 
     def add(self, digits: str, obj: any) -> None:
         self.head.add(obj, digits, 0)
 
     def find(self, digits: str) -> any:
         return self.head.find(digits, 0)
+    
+    # String formatting is used by print() and str().
+    def __str__(self) -> str:
+        return str(self.head.dicts)
