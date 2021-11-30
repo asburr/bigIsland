249456494bbd5d8f915d35c20ced9c1210518073
andrew <asburr@hotmail.com>
2021-11-30 05:52:01 -0500

    OOP for Cmds.

--- hallelujah/jah.py
+++ hallelujah/jah.py
@@ -12,191 +12,17 @@
 #
 
 from magpie.src.mudp import MUDP
-import tarfile
 import os.path
 import traceback
 # import ZipFile
 # import gzip
-import pandas as pd
-from discovery.src.CSVParser import CSVParser
-from discovery.src.JSONParser import JSONSchema
 from magpie.src.mworksheets import MWorksheets
 from magpie.src.mudp import MUDPBuildMsg
 import json
-from abc import ABC, abstractmethod
-import re
 import socket
+from cmds.files import Files
+# from cmds.loadf import Loadf
 from time import sleep
-
-
-class Cmd(ABC):
-    @abstractmethod
-    def headers(self) -> list:
-        raise Exception("Not implemented")
-
-    @abstractmethod
-    def schema(self) -> list:
-        raise Exception("Not implemented")
-
-    @abstractmethod
-    def sample(self, feedName: str, n:int) -> (bool, dict):
-        raise Exception("Not implemented")
-
-    @abstractmethod
-    def execute(self) -> None:
-        raise Exception("Not implemented")
-
-
-class Files(Cmd):
-    def __init__(self, cmd: dict):
-        self.path=cmd["path"]["path"]
-        self.depth=cmd["path"]["depth"]
-        self.files = set()
-        self.feeds = []
-        for feed in cmd["path"]["feeds"]:
-            self.feeds.append({
-                "feed": feed["feed"],
-                "regex": re.compile(feed["regex"], re.IGNORECASE),
-                "order": {},
-                "schema": JSONSchema()
-            })
-
-    def schema(self) -> list:
-        return ["stream", "filename"]
-
-    def sample(self, feedName: str, n:int) -> (bool, dict):
-        i = 0
-        if feedName:
-            for feed in self.feeds:
-                if feed["feed"] == feedName:
-                    for modifiedTime in feed["order"]:
-                        for pn in feed["order"][modifiedTime]:
-                            i += 1
-                            yield (i == n, pn)
-                            if i == n:
-                                return
-        else:
-            j = n / len(self.feeds)
-            if j == 0:
-                j = 1
-            kfeeds = {}
-            for feed in self.feeds:
-                f = feed["feed"]
-                kfeeds[f] = 0
-                for modifiedTime in feed["order"]:
-                    for pn in feed["order"][modifiedTime]:
-                        i += 1
-                        yield (i == n, {"feed": f, "file": pn})
-                        if kfeeds[f] == j:
-                            break
-                        kfeeds[f] += 1
-                        if i == n:
-                            return
-                    if kfeeds[f] == j:
-                        break
-            for feed in self.feeds:
-                f = feed["feed"]
-                j = 0
-                for modifiedTime in feed["order"]:
-                    for pn in feed["order"][modifiedTime]:
-                        if j > kfeeds[f]:
-                            i += 1
-                            yield (i == n,{"stream": f, "file": pn})
-                            if i == n:
-                                return
-
-    def execute(self) -> None:
-        self._scandir(self.depth)
-        # print(list(self.sample(None,10)))
-
-    def _scandir(self, depth: int) -> None:
-        for de in os.scandir(self.path):
-            if de.is_file():
-                sr = de.stat()
-                pn = os.path.join(de.path, de.name)
-                if pn not in self.files:
-                    for feed in self.feeds:
-                        if feed["regex"].match(pn):
-                            self.files.add(pn)
-                            feed["schema"].gather("", pn)
-                            feed["order"].setdefault(
-                                sr.st_mtime,[]).append(pn)
-                            break
-            elif de.is_dir() and depth > 0:
-                self._scandir(depth=depth-1)
-
-class Loadf(Cmd):
-    def __init__(self, cmd: dict):
-        self.fileHandlers = {
-            ".xls": self.__readxls,
-            ".__xls__": self.__read__xls__,
-            ".tar.gz": self.__readtargz,
-            '.__tar.gz__': self.__read__targz__
-        }
-
-    def schema(self) -> list:
-        return []
-
-    def sample(self, feedName: str, n:int) -> (bool, dict):
-        return
-
-    def execute(self) -> None:
-        return
-
-    # Finds the fileHandler to read the file from Network or Local disk.
-    def __readfile(self, fn: str, desc: dict) -> any:
-        (root, ext) = os.path.splitext(fn)
-        extentions = ""
-        while len(ext) > 0:
-            extentions = ext + extentions
-            if extentions in self.fileHandlers:
-                return self.fileHandlers(fn, desc)
-        return None
-
-    def __readxls(self, fn: str, desc: dict) -> any:
-        with pd.ExcelFile(fn) as e:
-            d = {"sheets": []}
-            for sn in e.sheet_names:
-                d["sheets"].append(sn+".__xls__")
-            return d
-
-    def __read__xls__(self, fn: str, desc: dict) -> any:
-        sn = fn[:-8]
-        return e.parse(sheet=sn).to_json()
-
-    def __readtargz(self, fn: str, desc: dict) -> any:
-        with tarfile.open(fn, "r:gz") as tar:
-            d = {}
-            for m in tar:
-                if m.isfile():
-                    d[m.name+".__tar.gz__"] = {
-                        "file": m.name,
-                        "tar": fn,
-                        "filesize": m.size,
-                        "offset": 0,
-                        "until": m.size
-                    }
-            return d
-
-    def __read__targz__(self, fn: str, desc: dict) -> any:
-        with tarfile.open(desc["tar"], "r:gz") as tar:
-            with tar.extractfile(desc["file"]) as f:
-                return self.__parse(
-                    f=f,
-                    offset=desc["offset"],
-                    until=desc["until"])
-        return None
-
-    def __parse(self, f: any, offset:int, until: int) -> any:
-        pass
-
-    def __parsetsv(self, f: any, offset:int, until: int) -> any:
-        return CSVParser("tsv").toJSON_FD(f,offset,until)
-
-    def __parsecsv(self, f: any, offset:int, until: int) -> any:
-        return CSVParser("csv").toJSON_FD(f,offset,until)
-
-
 
 class Jah():
     def __init__(self, identification: str, halleludir: str, worksheetdir: str):
@@ -236,25 +62,19 @@ class Jah():
         self.mudp = MUDP(socket=self.s, clientMode=False)
         print(str(self.port)+":"+self.id+" jah says hello " + str(self.ip))
 
-    def runCmd(self, remotecmd: any, requestId: int) -> bool:
-        if remotecmd is not None:
-            cmdname = MWorksheets.cmdName(remotecmd)
-            if cmdname == "_sample_":
-                self.sample(remotecmd, requestId)
-            else:
-                raise Exception("Unknown command " + cmdname)
-            return True
+    def runCmd(self, remotecmd: any, requestId: int) -> None:
+        cmdname = MWorksheets.cmdName(remotecmd)
+        if cmdname == "_sample_":
+            self.sample(remotecmd, requestId)
         else:
-            if self.cmdname == "files":
-                self.op.execute()
-            return True
-        return False
+            raise Exception("Unknown command " + cmdname)
 
     def sample(self, cmd: any, requestId: int) -> None:
         cmd = cmd["_sample_"]
         msg=MUDPBuildMsg(
                 remoteAddr=cmd["__remote_address__"],
                 requestId=requestId)
+        addrReqId=cmd["__remote_address__"] + (requestId,)
         self.mudp.send(
             content=json.dumps({
                 "_sample_response_": {
@@ -264,7 +84,7 @@ class Jah():
             eom=False, msg=msg
         )
         for eom, sample in self.op.sample( cmd["feed"], cmd["N"]):
-            if self.mudp.cancelledRequestId(requestId):
+            if self.mudp.cancelledRequestId(addrReqId):
                 break
             self.mudp.send(
                 content=json.dumps(sample),
@@ -280,6 +100,7 @@ class Jah():
                 self.runCmd(cmd, requestId)
             if not didSomething:
                 sleep(0.1)
+                self.op.execute()
         for p in self.processes:
             # Wait for child to terminate
             p.join()
